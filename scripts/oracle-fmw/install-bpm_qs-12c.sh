1e6e0898f946ef9e06208e94e025851bed794d6e
Jorge Quilcate <quilcate.jorge@gmail.com>
2015-11-22 23:01:22 +0100

    BPM 12.2.1 provisioning completed

---
+++ scripts/oracle-fmw/install-bpm_qs-12c.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+mkdir /home/$OS_USER/.inventory
+chown $OS_USER:$OS_GROUP -R /home/$OS_USER/.inventory
+
+cat > /tmp/install.file <<EOF
+[ENGINE]
+
+#DO NOT CHANGE THIS.
+Response File Version=1.0.0.0.0
+
+[GENERIC]
+
+#The oracle home location. This can be an existing Oracle Home or a new Oracle Home
+ORACLE_HOME=$ORACLE_HOME
+EOF
+
+cat > /tmp/oraInst.loc <<EOF
+inventory_loc=/home/$OS_USER/.inventory
+inst_group=$OS_GROUP
+EOF
+
+cd $BPM_INSTALL_DIR
+jar xf $BPM_ZIP
+jar xf $BPM_ZIP2
+su $OS_USER -c "java -jar $BPM_PKG -ignoreSysPrereqs -novalidation -silent -responseFile /tmp/install.file -invPtrLoc /tmp/oraInst.loc -jreLoc $JAVA_HOME"
+
+
+echo "ORACLE_HOME=$ORACLE_HOME; export ORACLE_HOME" >> /home/$OS_USER/.bash_profile
